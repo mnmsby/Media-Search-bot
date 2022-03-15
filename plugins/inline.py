b718ec868e9cfc09031ff704490f47978c2621fb
mnmsby <91547882+mnmsby@users.noreply.github.com>
2022-03-15 11:40:08 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -16,7 +16,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You have to join my channel to use me...',
                            switch_pm_parameter="subscribe")
         return
 
@@ -90,8 +90,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('Share Me 🔊', url='http://t.me/share/url?url=https://t.me/Unicodepro_bot')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
